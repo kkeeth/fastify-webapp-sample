c0a279270c31fdf707db407571422947f04e3fcd
Keeth Kuwahara <kiyohito.kuwahara@kaminashi.com>
2024-08-21 23:51:13 +0900

    feat: add test scenario about login

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -4,3 +4,12 @@ Scenario('example.comにアクセスする', ({I}) => {
   I.amOnPage('http://example.com');
   I.see('Example Domain');
 });
+
+Scenario('Webサイトを開きログインする', ({I}) => {
+  I.amOnPage('https://fastify-webapp-sample.takuyasuemura.dev/');
+  I.click('ログインする');
+  I.fillField('ユーザー名', 'user1');
+  I.fillField('パスワード', 'super-strong-passphrase');
+  I.click('ログイン');
+  I.see('user1 さん');
+});
